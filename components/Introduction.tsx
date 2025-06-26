068f712be6b81a286771282f548c4bb2bd54d65a
Jayalekshmi Rajeswari <jayalekshmirajeswarijoy@gmail.com>
2025-06-26 14:41:27 +0000

    Updating string literal

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -11,7 +11,7 @@ export default function Introduction() {
     >
       <div className="max-w-3xl p-2 lg:p8">
         <h1 className="text-2xl lg:text-5xl font-bold mb-2 lg:mb-5">
-          Hi, I'm Jayalekshmi 👋
+          {`Hi, I'm Jayalekshmi 👋`}
         </h1>
 
         <div className="mt-2 lg:mt-4 max-w-xl text-sm lg:text-xl flex flex-row mb-4 lg:mb-6 flex-wrap items-center justify-center lg:justify-start">
